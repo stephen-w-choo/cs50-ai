b624719fcbbf397d63d7938248a5a52359707bd1
stephen-w-choo <stephen.w.choo@gmail.com>
2022-10-18 10:38:35 +1100

    Partially completed - need to generalise terminals a bit better

--- parser/parser.py
+++ parser/parser.py
@@ -1,5 +1,6 @@
 import nltk
 import sys
+nltk.download('punkt')
 
 TERMINALS = """
 Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
@@ -14,8 +15,34 @@ V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
 V -> "smiled" | "tell" | "were"
 """
 
+"""
+sentence patterns:
+subject + verb
+subject + verb + object
+subject + verb + adverb
+subject + verb + noun
+
+
+Holmes sat down and lit his pipe.
+N V Adv
+
+I had a country walk on Thursday and came home in a dreadful mess.
+N V Det Adj N P N Conj V N P Det Adj N
+
+
+I had a little moist red paint in the palm of my hand.
+N V Det Adj Adj Adj N P Det N P Det N
+
+"""
+
+
 NONTERMINALS = """
-S -> N V
+S -> S | S P S
+SC -> NP V | NP V Adv
+AP -> Adj | Adj Adj | Adj Adj Adj
+VT -> V | AP V
+VP -> VT NP
+NP -> N | Det N | Det AP N
 """
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
@@ -70,7 +97,7 @@ def preprocess(sentence):
         newword = []
         for char in word:
             if char.isalpha():
-                newword.append(char)
+                newword.append(char.lower())
         if newword:
             res.append("".join(newword))
 
@@ -83,8 +110,30 @@ def np_chunk(tree):
     A noun phrase chunk is defined as any subtree of the sentence
     whose label is "NP" that does not itself contain any other
     noun phrases as subtrees.
+
+    Return value: list of nltk.tree objects, where each element has the label NP.
     """
-    raise NotImplementedError
+    res = []
+    # run a recursive DFS through the tree
+    # iterate down to the leaf nodes - on the return, return either True (if NP phrase has already been found)
+    # or False if NP phrase has not been found
+
+    def recursion(node):
+        found = False
+        
+        if len(node) > 0:
+            for subtree in node:
+                if recursion(subtree):
+                    found = True
+
+        # if NP phrase has not yet been found in a subtree, add the node if it's NP and set found to True
+        if not found and node.label() == "NP":
+            res.append(node)
+            found = True
+        
+        return found
+
+    return res
 
 
 if __name__ == "__main__":
